4277eca897dcff2630143449a6127a3bf1642143
Jisse Reitsma <info@yireo.com>
2015-11-03 21:35:51 +0100

    Pending changes

---
+++ source/app/code/community/Yireo/FormApi/Model/Form/Field/Products.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Yireo FormApi for Magento
+ *
+ * @package     Yireo_FormApi
+ * @author      Yireo (http://www.yireo.com/)
+ * @copyright   Copyright 2015 Yireo (http://www.yireo.com/)
+ * @license     Open Source License (OSL v3)
+ */
+
+class Yireo_FormApi_Model_Form_Field_Products extends Yireo_FormApi_Model_Form_Field_Input
+{
+    /**
+     * Template file of this form field
+     *
+     * @var string
+     */
+    protected $template = 'formapi/field/products.phtml';
+}
